bc50b2f738ed61357de08ea972356b85862eed8c
oop1qu3 <144473528+oop1qu3@users.noreply.github.com>
2024-04-30 10:09:51 +0900

    Create BOJ_2606.cpp

---
+++ 2024-1/Basic/oop1qu3/Graph/BOJ_2606.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define fastio std::cin.tie(0)->sync_with_stdio(0);
+using namespace std;
+
+vector<int> adj[101];
+bool vis[101];
+
+int main()
+{
+	fastio;
+
+	int N, M; cin >> N >> M;
+	while (M--)
+	{
+		int u, v; cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+
+	queue<int> que;
+	que.push(1);
+	while (!que.empty())
+	{
+		int cur = que.front(); que.pop();
+		for (int nxt : adj[cur])
+		{
+			if (vis[nxt]) continue;
+			que.push(nxt);
+			vis[nxt] = 1;
+		}
+	}
+
+	int count = 0;
+	for (int i = 2; i <= N; i++)
+	{
+		if (vis[i]) count++;
+	}
+
+	cout << count;
+	return 0;
+}

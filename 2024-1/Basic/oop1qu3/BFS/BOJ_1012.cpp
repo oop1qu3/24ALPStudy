c99cc66ba43367302c0e1d879fcd3a3785ba603f
oop1qu3 <144473528+oop1qu3@users.noreply.github.com>
2024-04-30 16:34:09 +0900

    Create BOJ_1012.cpp

---
+++ 2024-1/Basic/oop1qu3/BFS/BOJ_1012.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#define fastio std::cin.tie(0)->sync_with_stdio(0);
+using namespace std;
+
+typedef pair<int, int> pii;
+
+const int SIZE = 50;
+bool board[SIZE][SIZE];
+bool vis[SIZE][SIZE];
+queue<pii> que;
+
+int M, N, K;
+int dx[4] = { 0, 0, -1, 1 };
+int dy[4] = { -1, 1, 0, 0 };
+
+inline void test();
+void bfs(pii);
+
+int main()
+{
+	fastio;
+
+	int T; cin >> T;
+	while (T--)
+		test();
+}
+
+void test()
+{
+	cin >> M >> N >> K;
+
+	while (K--)
+	{
+		int X, Y; cin >> X >> Y;
+		board[Y][X] = 1;
+	}
+
+	int count = 0;
+	for (int i = 0; i < N; i++) for (int j = 0; j < M; j++)
+	{
+		if (board[i][j] == 1 && vis[i][j] == false)
+		{
+			bfs({ i, j });
+			count++;
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		fill(board[i], board[i] + M, 0);
+		fill(vis[i], vis[i] + M, false);
+	}
+
+	cout << count << "\n";
+}
+
+void bfs(pii start)
+{
+	que.push(start);
+	vis[start.first][start.second] = true;
+
+	while (!que.empty())
+	{
+		pii cur = que.front(); que.pop();
+
+		for (int i = 0; i < 4; i++)
+		{
+			int ny = cur.first + dy[i];
+			int nx = cur.second + dx[i];
+			if ((0 <= nx && nx < M) && (0 <= ny && ny < N) && 
+				(board[ny][nx] == 1) && (vis[ny][nx] == false))
+			{
+				que.push({ ny, nx });
+				vis[ny][nx] = true;
+			}
+		}
+	}
+}
